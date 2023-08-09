d8c58746a0a77b295aa0e6bc8851bf2f5696a442
zahid-soft <zahidsarang95@gmail.com>
2023-08-09 23:51:45 +0530

    product list page is created and header mobile view bug fixed

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,4 +1,7 @@
 import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
+import Footer from "../footer/Footer";
+import MobileNavbar from "../mobileNavbar/MobileNavbar";
 
 import { Link } from "react-router-dom";
 
@@ -8,11 +11,11 @@ import Cart from "../../utils/Cart";
 const Header = ({ children }) => {
   return (
     <div>
-      <header className="w-full h-34 lg:h-20 bg-gray-100 sticky top-0 z-50 border-b-[1px] border-b-gray-200 pb-2">
+      <header className="w-full h-34 lg:h-20 bg-gray-100  top-0 z-50 border-b-[1px] border-b-gray-200 pb-2">
         <nav className="relative h-full px-4 mx-auto max-w-container">
           {/* Logo */}
           <div className="flex flex-col items-center justify-between h-full lg:flex-row">
-            <Link to="">
+            <Link to="/">
               <div className=" my-5 lg:my-0 lg:mt-0 font-bold text-[.7rem] lg:font-extrabold lg:text-[2rem]">
                 <img src={Name} alt="Logo" />
               </div>
@@ -20,11 +23,11 @@ const Header = ({ children }) => {
 
             {/* search */}
             <div
-              className="mb-1 lg:mb-0 lg:mt-0 flex relative  w-full lg:w-[600px] h-[50px] text-base text-[#262626 ]
+              className="  mb-1 lg:mb-0 lg:mt-0 flex relative  w-full lg:w-[600px] h-[50px] text-base text-[#262626 ]
           bg-white  items-center gap-2 justify-between px-6 rounded-xl "
             >
               <input
-                className="flex-1 h-full bg-white outline-none placeholder:text-[#C4C4C4] placeholder:text-[14px"
+                className="border-0 flex-1 h-full bg-white outline-none placeholder:text-[#C4C4C4] placeholder:text-[14px"
                 type="text"
                 placeholder="search"
               />
@@ -33,8 +36,8 @@ const Header = ({ children }) => {
             {/* cart button and sign in button */}
             <div className="justify-end flex-shrink-0 hidden item-center lg:flex gap-x-6 lg:gap-x-8 2xl:gap-x-10 ltr:ml-auto rtl:mr-auto">
               <div className="-mt-0.5 flex-shrink-0">
-                <button className="text-sm font-semibold xl:text-base text-heading">
-                  Sign In
+                <button className="font-semibold text-white xl:text-black ">
+                  <PersonOutlineOutlinedIcon style={{fontSize:'2rem'}}/>
                 </button>
               </div>
               <Cart />
@@ -44,6 +47,8 @@ const Header = ({ children }) => {
       </header>
       {/* Home page content */}
       <main className="relative flex-grow ">{children}</main>
+      <Footer />
+      <MobileNavbar />
     </div>
   );
 };
--- client/src/components/homeContent/HomeContent.jsx
+++ client/src/components/homeContent/HomeContent.jsx
@@ -8,8 +8,7 @@ import banner5 from "../../assets/banner-5.webp";
 import banner6 from "../../assets/banner-6.webp";
 import Category from "../category/Category";
 import Featured from "../featuredProduct/Featured";
-import Footer from "../footer/Footer";
-import MobileNavbar from "../mobileNavbar/MobileNavbar";
+
 
 const HeroSection = () => {
   return (
@@ -17,7 +16,7 @@ const HeroSection = () => {
       <div className="mb-12 md:mb-14 xl:mb-16 px-2.5 grid grid-cols-2 sm:grid-cols-9 gap-2 md:gap-2.5 max-w-[1920px] mx-auto ">
         <div className="mx-auto col-span-full sm:col-span-5">
           <Link
-            to="/"
+            to="/productlist"
             className="relative flex justify-center h-full overflow-hidden group"
           >
             <img src={banner} alt="Men's Collection" />
@@ -66,8 +65,7 @@ const HeroSection = () => {
         <Category />
         <Featured />
       </div>
-      <Footer />
-      <MobileNavbar />
+      
     </div>
   );
 };
